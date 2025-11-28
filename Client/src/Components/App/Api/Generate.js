68628436cfd02290fd31b2f1682a7b20fea713d6
mj <shaikhm.ja.z@gmail.com>
2025-11-28 22:51:54 +0530

    fixing final bug

--- Client/src/Components/App/Api/Generate.js
+++ Client/src/Components/App/Api/Generate.js
@@ -38,10 +38,9 @@ export const fetchMeals = async () => {
 };
 export const getRecipeByMealId = async (mealId) => {
   try {
-    const res = await axios.get(
-      `${url}/api/crud/recipe/${mealId}`,
-      { withCredentials: true }
-    );
+    const res = await axios.get(`${url}/api/crud//get-recipe/${mealId}`, {
+      withCredentials: true,
+    });
     return res.data.recipe; // return ONLY the recipe
   } catch (err) {
     console.error("Error:", err);
