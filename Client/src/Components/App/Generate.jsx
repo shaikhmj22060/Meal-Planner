544a681b593c9ba2124fbc0af5f8c07dbcde74cd
mj <shaikhm.ja.z@gmail.com>
2025-11-28 18:49:05 +0530

    changes

--- Client/src/Components/App/Generate.jsx
+++ Client/src/Components/App/Generate.jsx
@@ -81,7 +81,7 @@ export default function Generate() {
       </motion.div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2">
         
-        <Card img={"/dish.svg"} tittle={Meal.meal.mealName} servings={Meal.meal.serving}/>
+        {/* <Card img={"/dish.svg"} tittle={Meal.meal.mealName} servings={Meal.meal.serving}/> */}
         <Card img={"/dish2.svg"} />
       </div>
 
