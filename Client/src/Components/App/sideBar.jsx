541940570efc615a9dc8dbcaf42de5100a51736b
mj <shaikhm.ja.z@gmail.com>
2025-11-28 23:46:56 +0530

    ui fix

--- Client/src/Components/App/sideBar.jsx
+++ Client/src/Components/App/sideBar.jsx
@@ -55,6 +55,9 @@ export default function Sidebar() {
       initial="hidden"
       animate="show"
     >
+      <Link className="md:hidden" to={"/"}>
+        {"<--"}
+      </Link>
       <motion.div
         className="flex items-center justify-between"
         variants={itemVariants}
