2f10caa0bd7460f3eb211043b4dfa12ca65d53c1
mj <shaikhm.ja.z@gmail.com>
2025-11-28 19:42:34 +0530

    useeffect aded

--- Client/src/Components/App/Generate.jsx
+++ Client/src/Components/App/Generate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import Button from "../Button";
 import Card from "./Card";
